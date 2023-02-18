abe119766582814a9668245897668dc87c2c5811
Vexoa <90162492+VexoaXYZ@users.noreply.github.com>
2023-02-18 19:09:33 +0000

    PATCH 1.1
    - Upgraded to work with server.lua

--- client.lua
+++ client.lua
@@ -1,81 +1,63 @@
 -- Function to display a notification to the player
 function ShowNotification(text)
-SetNotificationTextEntry("STRING")
-AddTextComponentString(text)
-DrawNotification(false, false)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, false)
 end
 
--- Event handler to listen for the "weed" and "cannabis" commands
+-- Event handler to listen for the "DisplayNotification" event
 RegisterNetEvent("DisplayNotification")
 AddEventHandler("DisplayNotification", function(text)
--- Display the notification to the player
-ShowNotification(text)
+    -- Display the notification to the player
+    ShowNotification(text)
 end)
 
 -- Register the "weed" command
-local isWeedEnabled = false
-RegisterCommand("weed", function()
--- Toggle the "weed" command on/off
-isWeedEnabled = not isWeedEnabled
-        if isWeedEnabled then
+RegisterCommand("weed", function(source, args, rawCommand)
     -- Display command information to the player
     ShowNotification("You used the /weed command, nearby players have been notified of a scent of weed.")
 
     -- Trigger the server event to notify nearby players
     local playerPos = GetEntityCoords(PlayerPedId())
     TriggerServerEvent("NotifyNearbyPlayers", "Weed", playerPos)
-else
-    -- Display command information to the player
-    ShowNotification("You turned off the /weed command, nearby players will no longer be notified of a scent of weed.")
-
-    -- Trigger the server event to remove the scent of weed
-    TriggerServerEvent("RemoveScent", "Weed")
-end
 end, false)
 
 -- Register the "cannabis" command
-local isCannabisEnabled = false
-RegisterCommand("cannabis", function()
--- Toggle the "cannabis" command on/off
-isCannabisEnabled = not isCannabisEnabled
-if isCannabisEnabled then
+RegisterCommand("cannabis", function(source, args, rawCommand)
     -- Display command information to the player
     ShowNotification("You used the /cannabis command, nearby players have been notified of a scent of cannabis.")
 
     -- Trigger the server event to notify nearby players
     local playerPos = GetEntityCoords(PlayerPedId())
     TriggerServerEvent("NotifyNearbyPlayers", "Cannabis", playerPos)
-else
-    -- Display command information to the player
-    ShowNotification("You turned off the /cannabis command, nearby players will no longer be notified of a scent of cannabis.")
-
-    -- Trigger the server event to remove the scent of cannabis
-    TriggerServerEvent("RemoveScent", "Cannabis")
-end
 end, false)
 
-
--- Register the "cannabis" command
-local isAlcoholEnabled = false
-RegisterCommand("alcohol", function()
--- Toggle the "cannabis" command on/off
-isCannabisEnabled = not isCannabisEnabled
-if isCannabisEnabled then
+-- Register the "alcohol" command
+RegisterCommand("alcohol", function(source, args, rawCommand)
     -- Display command information to the player
-    ShowNotification("You used the /alcohol command, nearby players have been notified of a scent of cannabis.")
+    ShowNotification("You used the /alcohol command, nearby players have been notified of a scent of alcohol.")
 
     -- Trigger the server event to notify nearby players
     local playerPos = GetEntityCoords(PlayerPedId())
     TriggerServerEvent("NotifyNearbyPlayers", "Alcohol", playerPos)
-else
-    -- Display command information to the player
-    ShowNotification("You turned off the /cannabis command, nearby players will no longer be notified of a scent of Alcohol.")
-
-    -- Trigger the server event to remove the scent of cannabis
-    TriggerServerEvent("RemoveScent", "Cannabis")
-end
 end, false)
 
+-- Register the "toggle" command
+RegisterCommand("toggle", function(source, args, rawCommand)
+    local scentType = args[1]
 
-        
-        
+    -- Clear all previously stored scents
+    TriggerServerEvent("ClearAllScents")
+
+    -- Display command information to the player
+    ShowNotification("You used the /toggle command, nearby players have been notified of a scent of "..scentType)
+
+    -- Trigger the server event to notify nearby players
+    local playerPos = GetEntityCoords(PlayerPedId())
+    TriggerServerEvent("NotifyNearbyPlayers", scentType, playerPos)
+end, false)
+
+-- Function to print a message when the script starts
+Citizen.CreateThread(function()
+    print("Thanks for using Weed/Cannabis Script By Creative Solutions")
+end)
