87b317399d96bb4305321e807771930ee7cf45dd
Vexoa <90162492+VexoaXYZ@users.noreply.github.com>
2023-02-18 19:03:51 +0000

    Update PATCH 1
    - Added off and on function for the script.

--- client.lua
+++ client.lua
@@ -1,46 +1,81 @@
--- Client-side code
-
 -- Function to display a notification to the player
 function ShowNotification(text)
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawNotification(false, false)
-  end
-  
-  -- Event handler to listen for the "weed" and "cannabis" commands
-  RegisterNetEvent("DisplayWeedNotification")
-  AddEventHandler("DisplayWeedNotification", function(location)
-    -- Display the notification to the player
-    ShowNotification("There's a scent of weed/cannabis nearby at "..location)
-  end)
-  
-  -- Register the "weed" command
-  RegisterCommand("weed", function(source, args, rawCommand)
+SetNotificationTextEntry("STRING")
+AddTextComponentString(text)
+DrawNotification(false, false)
+end
+
+-- Event handler to listen for the "weed" and "cannabis" commands
+RegisterNetEvent("DisplayNotification")
+AddEventHandler("DisplayNotification", function(text)
+-- Display the notification to the player
+ShowNotification(text)
+end)
+
+-- Register the "weed" command
+local isWeedEnabled = false
+RegisterCommand("weed", function()
+-- Toggle the "weed" command on/off
+isWeedEnabled = not isWeedEnabled
+        if isWeedEnabled then
     -- Display command information to the player
     ShowNotification("You used the /weed command, nearby players have been notified of a scent of weed.")
-  
+
     -- Trigger the server event to notify nearby players
     local playerPos = GetEntityCoords(PlayerPedId())
     TriggerServerEvent("NotifyNearbyPlayers", "Weed", playerPos)
-  end, false)
-  
-  -- Register the "cannabis" command
-  RegisterCommand("cannabis", function(source, args, rawCommand)
+else
+    -- Display command information to the player
+    ShowNotification("You turned off the /weed command, nearby players will no longer be notified of a scent of weed.")
+
+    -- Trigger the server event to remove the scent of weed
+    TriggerServerEvent("RemoveScent", "Weed")
+end
+end, false)
+
+-- Register the "cannabis" command
+local isCannabisEnabled = false
+RegisterCommand("cannabis", function()
+-- Toggle the "cannabis" command on/off
+isCannabisEnabled = not isCannabisEnabled
+if isCannabisEnabled then
     -- Display command information to the player
     ShowNotification("You used the /cannabis command, nearby players have been notified of a scent of cannabis.")
-  
+
     -- Trigger the server event to notify nearby players
     local playerPos = GetEntityCoords(PlayerPedId())
     TriggerServerEvent("NotifyNearbyPlayers", "Cannabis", playerPos)
-  end, false)
-  
-  -- Register the "alcohol" command
-  RegisterCommand("alcohol", function(source, args, rawCommand)
+else
     -- Display command information to the player
-    ShowNotification("You used the /alcohol command, nearby players have been notified of a scent of alcohol.")
-  
+    ShowNotification("You turned off the /cannabis command, nearby players will no longer be notified of a scent of cannabis.")
+
+    -- Trigger the server event to remove the scent of cannabis
+    TriggerServerEvent("RemoveScent", "Cannabis")
+end
+end, false)
+
+
+-- Register the "cannabis" command
+local isAlcoholEnabled = false
+RegisterCommand("alcohol", function()
+-- Toggle the "cannabis" command on/off
+isCannabisEnabled = not isCannabisEnabled
+if isCannabisEnabled then
+    -- Display command information to the player
+    ShowNotification("You used the /alcohol command, nearby players have been notified of a scent of cannabis.")
+
     -- Trigger the server event to notify nearby players
     local playerPos = GetEntityCoords(PlayerPedId())
     TriggerServerEvent("NotifyNearbyPlayers", "Alcohol", playerPos)
-  end, false)
-  
+else
+    -- Display command information to the player
+    ShowNotification("You turned off the /cannabis command, nearby players will no longer be notified of a scent of Alcohol.")
+
+    -- Trigger the server event to remove the scent of cannabis
+    TriggerServerEvent("RemoveScent", "Cannabis")
+end
+end, false)
+
+
+        
+        
